d0fc76fb8302c7089cb5ea277f60e69105935f18
stealmh <kmh922@naver.com>
2024-06-10 00:04:58 +0900

    [feat] #1 APIService 추가

---
+++ Projects/CoreKit/Sources/Network/APIService.swift
@@ -0,0 +1,35 @@
+//
+//  APIService.swift
+//  CoreKit
+//
+//  Created by 김민호 on 6/9/24.
+//
+
+import Foundation
+import ThirdPartyLib
+import Dependencies
+
+extension DependencyValues {
+    public var apiService: APIService {
+        get { self[APIService.self] }
+        set { self[APIService.self] = newValue }
+    }
+}
+
+public struct APIService {
+    public var requestLog: () -> Void
+}
+
+extension APIService: DependencyKey {
+    public static let liveValue: Self = {
+        return Self(
+            requestLog: { print("hello") }
+        )
+    }()
+    
+    public static let previewValue: Self = {
+       Self(
+        requestLog: { print("hello (only preview)") }
+       )
+    }()
+}
