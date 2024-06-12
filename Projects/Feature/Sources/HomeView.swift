bdf2ac7dfdd6c24fc4a0e0e34cbe6a4fa2397b3e
김도형 <108233361+ShapeKim98@users.noreply.github.com>
2024-06-12 16:20:07 +0900

    [chore] #3 불필요한 import 삭제

--- Projects/Feature/Sources/HomeView.swift
+++ Projects/Feature/Sources/HomeView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import ThirdPartyLib
 import ComposableArchitecture
 
 public struct HomeView: View {
--- Projects/Feature/Sources/ResultPopupView.swift
+++ Projects/Feature/Sources/ResultPopupView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import ThirdPartyLib
 import ComposableArchitecture
 
 struct ResultPopupView: View {
