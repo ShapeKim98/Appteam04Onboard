063e0cfad9e45ed46ad27b05c4f3b90ce757f406
김도형 <108233361+ShapeKim98@users.noreply.github.com>
2024-06-12 17:49:53 +0900

    [feat] #3 CoreNetwork 모듈 추가

--- Projects/CoreKit/Project.swift
+++ Projects/CoreKit/Project.swift
@@ -1,6 +1,18 @@
 import ProjectDescription
 import ProjectDescriptionHelpers
 
+let networkTarget = Target.target(
+    name: "CoreNetwork",
+    destinations: .appDestinations,
+    product: .staticLibrary,
+    bundleId: "\(String.moduleBundleId(name: "CoreKit")).CoreNetwork",
+    deploymentTargets: .appMinimunTarget,
+    sources: ["Sources/Network/**"],
+    dependencies: [
+        .project(target: "ThirdPartyLib", path: .relativeToRoot("Projects/ThirdPartyLib")),
+        .project(target: "Util", path: .relativeToRoot("Projects/Util"))
+    ])
+
 let project = Project(
     name: "CoreKit",
     targets: [
@@ -15,9 +27,9 @@ let project = Project(
             ),
             sources: ["Sources/**"],
             dependencies: [
-                .project(target: "ThirdPartyLib", path: .relativeToRoot("Projects/ThirdPartyLib")),
-                .project(target: "Util", path: .relativeToRoot("Projects/Util"))
+                .target(networkTarget)
             ]
-        )
+        ),
+        networkTarget
     ]
 )
